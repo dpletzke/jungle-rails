145d34d7f6e0f79e5fa0902cebb9f52350f9dd4a
daniel <dpletzke@gmail.com>
2020-11-11 23:19:54 -0600

    initalize admin categories view and controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,9 @@
+class Admin::CategoriesController < ApplicationController
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
+  
+  
+end
