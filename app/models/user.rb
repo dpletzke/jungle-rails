30703dd2b6a3ef8d61db16844b1d50bb70f8d304
daniel <dpletzke@gmail.com>
2020-11-18 00:21:58 -0600

    change login input to downcase for case-insensitive check, should enfore downcase in db

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email.strip)
+    user = User.find_by_email(email.strip.downcase)
 
     if user.authenticate(password)
       user
